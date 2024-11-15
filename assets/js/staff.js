7bfafc5d2bd13aedfa6955dea8c6edf56eb85618
CHAMUD12 <chamudgamage@gmail.com>
2024-11-15 18:10:54 +0530

    staff form getAll table vehicle code update

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -221,7 +221,7 @@ $("#getAllBtn").click(function () {
             <td>${staff.addressLine05 || "N/A"}</td>
             <td>${staff.contactNo}</td>
             <td>${staff.email}</td>
-            <td>${staff.vehicle ? staff.vehicle.vehicleCode : "N/A"}</td>
+            <td>${staff.vehicleCode}</td>
           </tr>
         `;
       });
